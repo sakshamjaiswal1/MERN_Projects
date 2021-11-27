628340db7419ece346d4f582e5fced35b5507b19
Saksham Jaiswal <sjais973@gmail.com>
2021-11-27 14:10:57 +0530

    new

--- TravelApp_lama/server/index.js
+++ TravelApp_lama/server/index.js
@@ -19,9 +19,13 @@ mongoose
   .catch((err) => console.log(err));
 app.use('/api/pins',pinRoute)
 app.use('/api/users',userRoute)
+app.use(express.static(path.join(__dirname, "/client")));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
   
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend is running");
 });
