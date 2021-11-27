a3d9d077c01f74b2a36bad5e576e024badb3ac07
Saksham Jaiswal <sjais973@gmail.com>
2021-11-27 19:40:36 +0530

    new

--- Blog_WebApp/api/index.js
+++ Blog_WebApp/api/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+dotenv.config()
 
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 app.use('/',(req,res)=>{
     console.log('this is main Url')
 })
